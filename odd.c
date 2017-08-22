a4179e52470882c977f0b73f1e03a87c1e8cd0d7
vanmathi12 <30343291+vanmathi12@users.noreply.github.com>
2017-08-22 16:03:25 +0530

    Update and rename even or odd to odd.c

---
+++ odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+int number;
+printf("enter the number");
+scanf("%d", &number);
+if(number %2==0);
+printf("%d number is even",number);
+else
+printf("%d number is odd",number);
+return 0;
+}
